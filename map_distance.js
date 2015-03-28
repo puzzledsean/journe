ed736c4b24d5d72c4545906f11cce02572dffe6b
puzzledsean <puzzledsean@gmail.com>
2015-03-28 19:52:19 -0400

    successfully got element from time div to work with length calculator

--- map_distance.js
+++ map_distance.js
@@ -77,7 +77,8 @@ function callback(response, status) {
         timeDiv.innerHTML = 'Travel time: ' + results[j].duration.text
         hiddenDiv.value = convertToMS(results[j].duration.text)
         alert(hiddenDiv.value)
-
+        hiddenDiv.innerHTML = hiddenDiv.value;
+        display_dictionary();
       }
     }
   }
@@ -125,6 +126,30 @@ function convertToMS(timeString) {
     }
   }
   return seconds * 1000;
+}
+
+function display_dictionary(genre){
+    var myjson;
+    var blah = "misery";
+    $.getJSON("https://api.spotify.com/v1/search?query=track%3A%22" + blah + "%22&offset=0&limit=50&type=track", function(json){
+        myjson = json;
+        console.log(myjson);
+
+    counter = 0;
+    iteration = 0;
+    tripLength = document.getElementById('time').innerText;
+    for(i = 0; i <50; i++){
+        current = Math.floor(Math.random() * 49);
+        if(counter + myjson.tracks.items[current].duration_ms > tripLength){
+            ;
+        }
+        else{
+            counter += myjson.tracks.items[current].duration_ms;
+            iteration = i;
+        }
+    }
+    console.log("the total song times add up to " + counter + " having gone through " + iteration + " songs when the length of the trip is " + tripLength);
+    });
 }
 
 
